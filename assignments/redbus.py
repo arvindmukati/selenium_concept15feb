29e5e8424cfb3b1562939cedebce1fd58ca68cc7
arvindmukati <arvindmukatee@gmail.com>
2023-03-02 18:14:47 +0530

    Redbus Assignment

--- assignments/redbus.py
+++ assignments/redbus.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,5 +9,11 @@ driver.implicitly_wait(30)
 driver.get("https://www.redbus.in/")
 driver.find_element(By.ID,"i-icon-profile").click()
 driver.find_element(By.ID,"signInLink").click()
-driver.find_element(By.XPATH,"//div[@class='mobileInput clearfix']//input").send_keys("7898")
 
+driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@class='modalIframe']"))
+driver.find_element(By.XPATH,"//div[@class='mobileInput clearfix']//input").send_keys("7898")
+time.sleep(2)
+error_msg = driver.find_element(By.XPATH,"//span[@class='error-message-fixed error-message-full top-fix']").text
+print(error_msg)
+time.sleep(3)
+driver.quit
